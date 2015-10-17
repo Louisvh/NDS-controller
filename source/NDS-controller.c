eab42138ae04ee0dbb4bc65ff102b9a7465650c6
Louisvh <Louis.van.harten@gmail.com>
2015-10-17 18:12:36 +0200

    Initial commit Contains default Makefile and skeleton

---
+++ source/NDS-controller.c
@@ -0,0 +1,23 @@
+/*
+ * TODO add copyright notice and GPL licence thingey
+ *
+ *
+ *
+ */
+#include <nds.h>
+#include <dswifi9.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int i;
+
+	consoleDemoInit();
+
+
+	return 0;
+}
