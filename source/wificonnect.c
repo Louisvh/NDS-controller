b7ab65bbca6e8f59b583b644d6a33ac4c300779b
Louisvh <Louis.van.harten@gmail.com>
2015-10-22 01:17:42 +0200

    removed the WPA skipping code in the wifi browser. Cleaned things up a bit

--- source/wificonnect.c
+++ source/wificonnect.c
@@ -89,10 +89,9 @@ int ManualConnect(PrintConsole *top_screen, PrintConsole *bot_screen) {
 }
 
 Wifi_AccessPoint* findAP() {
-    int i,j=0,skip_n;
+    int i;
     int selected = 0;
-    int *fWPA_buff, *bWPA_buff;
-    int count = 0, displaytop = 0;
+    int count = 0, displaytop = 0, displayend;
     int pressed = 0;
     static Wifi_AccessPoint ap;
     Wifi_ScanMode();
@@ -101,34 +100,9 @@ Wifi_AccessPoint* findAP() {
         pressed = keysDown();
 
         count = Wifi_GetNumAP();
-        fWPA_buff = (int*) malloc(count*sizeof(int));
-        bWPA_buff = (int*) malloc(count*sizeof(int));
-        // check how many WPA connections there are at what places
-        for(i = count-1, skip_n = 0, j=0; i >= 0; i--) {
-            Wifi_AccessPoint tap;
-            Wifi_GetAPData(i, &tap);
-            if(tap.flags & WFLAG_APDATA_WPA) {
-                skip_n++;
-                j++; //track n WPA
-                fWPA_buff[i] = skip_n;
-            } else {
-                skip_n = 0;
-            }
-        }
-        for(i = 0, skip_n = 0; i < count; i++) {
-            Wifi_AccessPoint tap;
-            Wifi_GetAPData(i, &tap);
-            if(tap.flags & WFLAG_APDATA_WPA) {
-                skip_n++;
-                bWPA_buff[i] = skip_n;
-            } else {
-                skip_n = 0;
-            }
-        }
-
         consoleClear();
-        iprintf(" %d WEP/Open APs detected\n\n\n", count-j);
-        int displayend = displaytop + 6;
+        iprintf(" %d Available APs Detected \n\n\n\n\n", count);
+        displayend = displaytop + 4;
         if (displayend > count) {
             displayend = count;
         }
@@ -136,35 +110,36 @@ Wifi_AccessPoint* findAP() {
             Wifi_AccessPoint tap;
             Wifi_GetAPData(i, &tap);
             if(tap.flags & WFLAG_APDATA_WPA) {
-                continue;
+                iprintf(" %s %.28s\n    WPA Sig:%i\n\n", i == selected ? "-X" : "  ", tap.ssid,
+                 tap.rssi * 100 / 0xD0);
             } else {
-                iprintf(" %s %.28s\n %s Sig:%i\n\n", i == selected ? "->" : "  ", tap.ssid,
+                iprintf(" %s %.28s\n    %s Sig:%i\n\n", i == selected ? "->" : "  ", tap.ssid,
                         tap.flags & WFLAG_APDATA_WEP ? "WEP  " : "Open ", tap.rssi * 100 / 0xD0);
             }
         }
         if(pressed & KEY_UP) {
-            if(selected-1-bWPA_buff[selected -1]  >= 0) {
-                selected -= 1+bWPA_buff[selected -1];
+            if(selected > 0) {
+                selected--;
             }
             if(selected<displaytop) {
                 displaytop = selected;
             }
         }
         if(pressed & KEY_DOWN) {
-            if(selected+1+fWPA_buff[selected+1] < count) {
-                selected += 1+fWPA_buff[selected+1];
+            if(selected+1 < count) {
+                selected ++;
             }
-            displaytop = selected - 6;
+            displaytop = selected - 3;
             if (displaytop<0) {
                 displaytop = 0;
             }
         }
+        if(pressed & KEY_B) {
+            return NULL;
+        }
         swiWaitForVBlank();
-    } while(!(pressed & KEY_A));
-    Wifi_GetAPData(selected, &ap);
-
-    free(fWPA_buff);
-    free(bWPA_buff);
+        Wifi_GetAPData(selected, &ap);
+    } while(!(pressed & KEY_A) || ap.flags & WFLAG_APDATA_WPA);
 
     return &ap;
 }
